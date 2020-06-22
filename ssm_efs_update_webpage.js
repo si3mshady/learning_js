4216e825fc56f6503e3baa515735ae515d6a158f
Elliott Arnold <46078321+si3mshady@users.noreply.github.com>
2020-06-22 06:48:31 -0500

    Update ssm_efs_update_webpage.js

--- ssm_efs_update_webpage.js
+++ ssm_efs_update_webpage.js
@@ -1,9 +1,9 @@
 var AWS = require("aws-sdk");
 
 class EfsList {
-    constructor(instance_id='i-elliott-learns-js') {
+    constructor(instance_id='i-0a23e18c84b5d2c04') {
         this.instance_id = instance_id;
-        this.mount = "sudo mount -t nfs4 -o nfsvers=4..../ /mnt/"
+        this.mount = "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-88888888.efs.us-east-1.amazonaws.com:/ /mnt"
     }        
 
     get_params(cmd) {
@@ -29,8 +29,7 @@ class EfsList {
             AWS.config.update({region: 'us-east-1'})        
             const ssm = new AWS.SSM();
             await ssm.sendCommand(this.get_params('sudo ls /mnt'),(err,data) => {                                                      
-            const results = setTimeout(() =>  this.get_command_exec_results(data.Command.CommandId), \
-                                       10000);               
+            const results = setTimeout(() =>  this.get_command_exec_results(data.Command.CommandId),  10000);               
             
        });
 
@@ -43,6 +42,7 @@ class EfsList {
             await ssm.getCommandInvocation(params,(err,data) => {                    
                 if (data.StandardOutputContent) {      
                     // get html insert string          
+                    //console.log(data)
                     const file_list = data.StandardOutputContent.split('\n')
                     const new_array = file_list.map(curr => this.render_list(curr))                    
                     console.log(new_array.join(''))                                         
@@ -58,6 +58,11 @@ class EfsList {
         return html 
     }}};     
 
+
 //Elliott Arnold - learning js quick and dirty
 //Using JS to access data from EFS filesystem; genearate html sting to send to browser
 //6-22-20
+
+
+efs = new EfsList()
+efs.list_efs_share()
